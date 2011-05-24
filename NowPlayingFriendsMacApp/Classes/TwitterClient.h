ca2e844e53d7722a628657a0fecd7a7abc5f653e
HIROE Shin <hiroe.orz@gmail.com>
2011-05-24 11:14:11 +0900

    add JSON libraries and rename OAuthConsumer dir name

---
+++ NowPlayingFriendsMacApp/Classes/TwitterClient.h
@@ -0,0 +1,34 @@
+//
+//  TwitterClient.h
+//  NowPlayingFriendsMacApp
+//
+//  Created by Hiroe Shin on 11/05/24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TwitterClient+ConsumerKey.h"
+#import "OAuthConsumer/OAServiceTicket.h"
+
+
+#define kVerifyUrl @"https://api.twitter.com/1/account/verify_credentials.json"
+#define kOAuthTokenUrl @"https://api.twitter.com/oauth/access_token"
+#define kHomeTimelineURL @"https://twitter.com/statuses/home_timeline/%@.json%@"
+#define kUserTimelineURL @"https://twitter.com/statuses/user_timeline/%@.json%@"
+#define kMenthonsTimelineURL @"https://api.twitter.com/1/statuses/mentions.json%@"
+#define kSearchURL @"http://search.twitter.com/search.json?q=%@"
+#define kUserInformationURL @"https://api.twitter.com/1/users/show/%@.json"
+#define kUpdateStatusURL @"https://twitter.com/statuses/update.json"
+#define kOAuthAccetokenFileName @"access_token.plist"
+#define kCreateFriendURL @"https://twitter.com/friendships/create/%@.json"
+#define kCheckFriendShipURL @"https://api.twitter.com/1/friendships/show.json?target_screen_name=%@"
+
+#define kTwitterFrindsSearchUrl @"https://api.twitter.com/1/statuses/friends.json?screen_name=%@&cursor=%@"
+
+
+@interface TwitterClient : NSObject {
+@private
+    
+}
+
+@end
---
+++ NowPlayingFriendsMacApp/Classes/TwitterClient.m
@@ -0,0 +1,29 @@
+//
+//  TwitterClient.m
+//  NowPlayingFriendsMacApp
+//
+//  Created by Hiroe Shin on 11/05/24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwitterClient.h"
+
+
+@implementation TwitterClient
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
